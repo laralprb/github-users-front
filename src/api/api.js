a6f51fcd38ddee3df398fff6d562c541a4712880
lara barbosa <larapattylprb@gmail.com>
2023-04-26 13:11:00 -0300

    add production url

--- src/api/api.js
+++ src/api/api.js
@@ -2,6 +2,7 @@ import axios from 'axios';
 
 const apiURLs = {
   development: 'https://api.github.com/',
+  production: 'https://brainy-top-hat-bear.cyclic.app',
 };
 
 const api = axios.create({ baseURL: apiURLs[process.env.NODE_ENV] });
